70192bcc97de9973ce26023f64a771e531b9157d
giancode1 <gieg5c@gmail.com>
2021-09-14 12:29:00 -0500

    cambiado titulo 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GC PRUEBA VERCEL
+          GC PRUEBA VERCEL 1
         </p>
         <a
           className="App-link"
